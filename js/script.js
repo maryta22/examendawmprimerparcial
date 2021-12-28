294b60fdb5ff43a461dc9e8a98e0c80f88a81feb
maryta22 <maria.mariaog.rivera@gmail.com>
2021-12-28 18:33:35 -0500

    mejora después de revisión

--- js/script.js
+++ js/script.js
@@ -25,39 +25,27 @@
 
  */
 
-
-
 function responderEvento(identificador) {
 
   var URL = 'https://dataserverdaw.herokuapp.com/cotizaciones/' + identificador + '/json';
   var values = document.getElementById("values");
 
-  var plantilla = `<tr>
-  <td>"fecha"</td>
-  <td>"numero"</td>
-  <td class="clase">"variacion"</td>
-  </tr>`;
-
-  var plantillaNueva = ``;
+  var plantilla = ``;
 
   document.getElementById(identificador).addEventListener("mouseover", () => {
     const nombres = fetch(URL)
       .then(response => response.json())
       .then(data => {
         for (var i = 0; i < data.cotizaciones.length; i++) {
-          var plantillaProvicional = plantilla;
-          plantillaProvicional = plantillaProvicional.replace('"fecha"', data.cotizaciones[i].fecha);
-          plantillaProvicional = plantillaProvicional.replace('"numero"', data.cotizaciones[i].numero);
-          variacion = data.cotizaciones[i].variacion.replace(",", ".").replace("%", "");
-          if (parseFloat(variacion) > 0) {
-            plantillaProvicional = plantillaProvicional.replace('"variacion"', "up");
-          } else {
-            plantillaProvicional = plantillaProvicional.replace('"variacion"', "down");
-          }
-
-          plantillaNueva += plantillaProvicional;
+          plantilla += `
+          <tr>
+          <td>"${data.cotizaciones[i].fecha}"</td>
+          <td>"${data.cotizaciones[i].numero}"</td>
+          <td class="clase">"${data.cotizaciones[i].variacion.replace(",", ".").replace("%", "")>0?"up":"down"}"</td>
+          </tr>
+          `;
         }
-        values.innerHTML = plantillaNueva;
+        values.innerHTML = plantilla;
       });
   });
 }
